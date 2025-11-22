32efd75323cf59550b5883af1440121478fb7ddb
MtAkira <208238364+MtAkira@users.noreply.github.com>
2025-11-22 12:31:27 +0900

    フォルダとアセンブリの分離

--- 58Hack/Assets/Common/PointsData.cs
+++ 58Hack/Assets/Common/PointsData.cs
@@ -1,6 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PointsData
+public class PicturePoints
 {
-    
+    private Point[] _points;
+    private Vector2Int _resolution;
+    public PicturePoints(Point[] points, Vector2Int resolution)
+    {
+        _points = points;
+        _resolution = resolution;
+    }
+    public Point[] GetPoints()
+    {
+        return _points;
+    }
+    public Vector2Int GetResolution()
+    {
+        return _resolution;
+    }
+}
+public struct Point
+{
+    public Color color;
+    public Vector2 pos;
 }
